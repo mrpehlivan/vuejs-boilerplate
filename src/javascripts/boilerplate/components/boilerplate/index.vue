92c6d21eefabfa1eb1166a13656c3334d56d9522
Arif PEHLİVAN <m.arifpehlivan@gmail.com>
2016-11-23 14:31:40 +0300

    index page

--- src/javascripts/boilerplate/components/boilerplate/index.vue
+++ src/javascripts/boilerplate/components/boilerplate/index.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>
-        <p>You can see the message : {{ message }} </p>
+    <div class="container">
+        <img  style="header" src="/images/logo.png" />
+        <div>
+            <h1 style="margin-left:10%">{{ message }} </h1>
+
+            <input  class="clickButton" type="button" v-on:click="testMethod(message)" value="CLICK ME"/>
+        </div>
     </div>
-    <div>
-        <input type="button" v-on:click="testMethod(message)" value="Click me !"/>
-    </div>
+
+
 </template>
 <script>
     export default {
@@ -13,8 +17,7 @@
         },
         data: function () {
             return {
-                message:"hello world ! ",
-                secondMessage : "HELLO WORLD!"
+                message:"HELLO WORLD!",
 
             }
         },
@@ -26,5 +29,31 @@
     }
 </script>
 <style>
+.container{
+    background-color: #ffffff;
+    margin-left: 40%;
+}
+
+.header{
+    margin-left: 50px;
+}
+.clickButton {
+    background: #fff;
+    -webkit-appearance: none;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    -ms-border-radius: 4px;
+    -o-border-radius: 4px;
+    border-radius: 4px;
+    height: 40px;
+    line-height: 40px;
+    position: relative;
+    display: block;
+    width: 40%;
+    top: 50%;
+    color: #009CDE;
+    border: 1px solid #009CDE;
+    font-weight: 700;
+}
 
 </style>
